1fec1042ebc7fb5687fb53b7c8a1958f19294dde
Artur Demchuk <areb1gamesyt@gmail.com>
2026-05-21 22:34:22 +0300

    ShakeEffect update

--- Assets/Resources/Scripts/Player/Camera/Effects/ShakeOnShootEffect.cs
+++ Assets/Resources/Scripts/Player/Camera/Effects/ShakeOnShootEffect.cs
@@ -10,31 +10,32 @@ public class ShakeOnShootEffect : MonoBehaviour
     [SerializeField] private float _randomness;
 
     [Header("References")]
-    [SerializeField] private WeaponInventory _weaponInventory;
+    [SerializeField] private Gun _gun;
+    [SerializeField] private Transform _cameraTransform;
 
     private Vector3 _originalPosition;
 
     private void Shake()
     {
-        transform.DOKill();
-        transform.localPosition = _originalPosition;
+        _cameraTransform.DOKill();
+        _cameraTransform.localPosition = _originalPosition;
 
-        transform.DOShakePosition(_duration, _strength, _vibrato, _randomness)
-            .OnComplete(() => transform.localPosition = _originalPosition);
+        _cameraTransform.DOShakePosition(_duration, _strength, _vibrato, _randomness)
+            .OnComplete(() => _cameraTransform.localPosition = _originalPosition);
     }
 
     private void Awake()
     {
-        _originalPosition = transform.localPosition;
+        _originalPosition = _cameraTransform.localPosition;
     }
 
     private void OnEnable()
     {
-        _weaponInventory.SelectedWeapon.OnAttack += Shake;
+        _gun.OnAttack += Shake;
     }
 
     private void OnDisable()
     {
-        _weaponInventory.SelectedWeapon.OnAttack -= Shake;
+        _gun.OnAttack -= Shake;
     }
 }
