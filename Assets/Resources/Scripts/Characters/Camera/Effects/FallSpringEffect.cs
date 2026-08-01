c7e09a8fb36b42e0904283754e9ad0bccdd30080
Artur Demchuk <areb1gamesyt@gmail.com>
2026-08-02 01:18:01 +0300

    falling spring tensioin added

--- Assets/Resources/Scripts/Characters/Camera/Effects/FallSpringEffect.cs
+++ Assets/Resources/Scripts/Characters/Camera/Effects/FallSpringEffect.cs
@@ -7,12 +7,17 @@ public class FallSpringEffect : MonoBehaviour, IPositionEffect
     [Header("Spring Settings")]
     [SerializeField] private float _returnSpeed;
     [SerializeField] private float _shakeDamping;
-    [SerializeField] private float _effectWeight;   
+    [SerializeField] private float _effectWeight;
 
-    [Header("Dynamic Scaling")]
+    [Header("Landing Impact Settings")]
     [SerializeField] private float _maxForce;
     [SerializeField] private float _minFallSpeedThreshold;
     [SerializeField] private float _forceMultiplier;
+
+    [Header("In-Air Compression Settings")]
+    [SerializeField] private float _inAirCompressionForce;
+    [SerializeField] private float _maxInAirOffset;
+    [SerializeField] private float _fallingForceMultiplier;
 
     [Header("References")]
     [SerializeField] private CharacterEngine _characterEngine;
@@ -22,8 +27,34 @@ public class FallSpringEffect : MonoBehaviour, IPositionEffect
     private Vector3 _calculatedCameraOffset;
     private Vector3 _shakeVelocity;
     private float _capturedFallSpeed;
-    
+
     public Vector3 GetLocalOffset() => _calculatedCameraOffset;
+
+    private void Update()
+    {
+        float currentVerticalSpeed = _characterEngine.Velocity.y;
+
+        if (currentVerticalSpeed < 0 && !_groundCheck.IsGrounded)
+        {
+            _capturedFallSpeed = currentVerticalSpeed;
+
+            float fallRatio = Mathf.Abs(currentVerticalSpeed) / _minFallSpeedThreshold;
+            float compressionFactor = Mathf.Clamp01(fallRatio);
+
+            float targetAirOffset = -Mathf.Min(Mathf.Abs(currentVerticalSpeed) * _fallingForceMultiplier, _maxInAirOffset);
+
+            _calculatedCameraOffset.y = Mathf.Lerp(_calculatedCameraOffset.y, targetAirOffset, Time.deltaTime * _inAirCompressionForce);
+        }
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 returnForce = -_returnSpeed * _calculatedCameraOffset - _shakeDamping * _shakeVelocity;
+        Vector3 acceleration = returnForce / _effectWeight;
+
+        _shakeVelocity += acceleration * Time.deltaTime;
+        _calculatedCameraOffset += _shakeVelocity * Time.deltaTime;
+    }
 
     private void ApplyLandingForce()
     {
@@ -36,26 +67,15 @@ public class FallSpringEffect : MonoBehaviour, IPositionEffect
         _capturedFallSpeed = 0f;
     }
 
-    private void LateUpdate()
+    private void OnEnable()
     {
-        Vector3 returnForce = -_returnSpeed * _calculatedCameraOffset - _shakeDamping * _shakeVelocity;
-        Vector3 acceleration = returnForce / _effectWeight;
-
-        _shakeVelocity += acceleration * Time.deltaTime;
-        _calculatedCameraOffset += _shakeVelocity * Time.deltaTime;
+        if (_groundCheck != null)
+            _groundCheck.OnGrounded += ApplyLandingForce;
     }
 
-    private void Update()
+    private void OnDisable()
     {
-        float currentVerticalSpeed = _characterEngine.Velocity.y;
-
-        if (currentVerticalSpeed < 0)
-        {
-            _capturedFallSpeed = currentVerticalSpeed;
-        }
+        if (_groundCheck != null)
+            _groundCheck.OnGrounded -= ApplyLandingForce;
     }
-
-    private void OnEnable() => _groundCheck.OnGrounded += ApplyLandingForce;
-    
-    private void OnDisable() => _groundCheck.OnGrounded -= ApplyLandingForce;
 }
