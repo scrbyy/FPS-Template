3e5fea727323e95a38fe19964db92860c124e2c5
Artur Demchuk <areb1gamesyt@gmail.com>
2026-08-08 16:27:21 +0300

    created knife scripts

---
+++ Assets/Resources/Scripts/Characters/Weapon/Other/KnifeAttacker.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assets.Resources.Scripts.Characters.Weapon.Other
+{
+    internal class KnifeAttacker
+    {
+    }
+}
---
+++ Assets/Resources/Scripts/Characters/Weapon/ShootMethods/SpherecastAttack.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assets.Resources.Scripts.Characters.Weapon.ShootMethods
+{
+    internal class SpherecastAttack
+    {
+    }
+}
